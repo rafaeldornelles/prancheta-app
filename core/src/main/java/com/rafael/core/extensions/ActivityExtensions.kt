a393b1729ef39c477222a46112bb53c92381164b
Rafael Dornelles <rafael.dornelles@ilia.digital>
2022-11-14 20:43:11 -0300

    feat: encode photos before sending

--- core/src/main/java/com/rafael/core/extensions/ActivityExtensions.kt
+++ core/src/main/java/com/rafael/core/extensions/ActivityExtensions.kt
@@ -1,6 +1,5 @@
 package com.rafael.core.extensions
 
-import android.app.Activity
 import android.app.DatePickerDialog
 import android.content.Context
 import android.graphics.Bitmap
@@ -9,12 +8,8 @@ import android.os.Environment
 import android.util.Base64
 import android.util.Base64OutputStream
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.core.content.FileProvider
-import com.google.android.material.datepicker.MaterialDatePicker
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.text.SimpleDateFormat
@@ -40,7 +35,7 @@ fun Context.showDatePicker(
     picker.show()
 }
 
-fun Context.createImageFile() : File {
+fun Context.createImageFile(): File {
     val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
     val storageDir = this.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
     return File.createTempFile(
@@ -50,18 +45,19 @@ fun Context.createImageFile() : File {
     )
 }
 
-fun File.toBase64() : String {
-    return ByteArrayOutputStream().use { outputStream ->
-        Base64OutputStream(outputStream, Base64.DEFAULT).use { base64FilterStream ->
-            this.inputStream().use { inputStream ->
-                inputStream.copyTo(base64FilterStream)
-            }
-        }
-        return@use outputStream.toString()
-    }
+fun File.toBase64(maxSize: Int = 1024 * 1024): String {
+    val outputStream = ByteArrayOutputStream()
+    var bmp = BitmapFactory.decodeFile(this.absolutePath)
+    do {
+        bmp.compress(Bitmap.CompressFormat.JPEG, 50, outputStream)
+        Log.d("AAAA", outputStream.toByteArray().size.toString())
+        bmp = BitmapFactory.decodeByteArray(outputStream.toByteArray(), 0, outputStream.size())
+    } while (outputStream.toByteArray().size > maxSize)
+
+    return Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT)
 }
 
-fun String.toBitmap() : ImageBitmap {
+fun String.toBitmap(): ImageBitmap {
     val bytes = Base64.decode(this, Base64.DEFAULT)
     return BitmapFactory.decodeByteArray(bytes, 0, bytes.size).asImageBitmap()
 }
